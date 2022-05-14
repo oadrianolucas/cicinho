e136768e3fb2d8eeee36a61c4e978ae4118a4f4f
Adrino Lucas <contato@adrianolucas.com>
2022-05-14 12:46:20 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,52 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { FaInstagram, FaFacebook, FaWhatsapp } from 'react-icons/fa';
+import  { GiMagnifyingGlass } from 'react-icons/gi'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-blue-800">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cicinho - Pode confiar!</title>
+        <link rel="icon"/>
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center ">
+        <img className='w-48 m-4 rounded-full' src='/cicinho.png' />
+        <h1 className='text-xl font-bold text-white mb-2'>Vereador</h1>
+        <img className='w-96' src='/logo.svg' />
+        <p className="mt-3 text-2xl text-white font-bold">
+          Pode confiar!
         </p>
-
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://www.facebook.com/ocicinhooficial" target='_blank'
+            className="mt-6 w-96 rounded text-2xl font-bold bg-white p-3 flex text-blue-800 hover:text-blue-900 hover:bg-gray-100"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
+            <FaFacebook className='mt-1 mr-2'/> Facebook 
           </a>
-
           <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://www.instagram.com/ocicinho_/" target='_blank'
+            className="mt-6 w-96 rounded text-2xl font-bold bg-white p-3 flex text-blue-800 hover:text-blue-900 hover:bg-gray-100"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
+            <FaInstagram className='mt-1 mr-2'/> Instagram 
           </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="#" target='_blank' 
+            className="mt-6 w-96 rounded text-2xl font-bold bg-white p-3 flex text-blue-800 hover:text-blue-900 hover:bg-gray-100"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
+            <FaWhatsapp className='mt-1 mr-2'/> WhatsApp 
           </a>
-
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://www.cmdiadema.sp.gov.br/index.php?exe=gabinete&codedil=edil04" target='_blank'
+            className="mt-6 w-96 rounded text-2xl font-bold bg-white p-3 flex text-blue-800 hover:text-blue-900 hover:bg-gray-100"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <GiMagnifyingGlass className='mt-1 mr-2'/> Confira nosso trabalho 
           </a>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex h-24 w-full items-center justify-center text-white">
+        <p>Copyright © 2022 Cicinho.</p>
       </footer>
     </div>
   )
